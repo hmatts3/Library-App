96b0395d4627f4ea843e8c80303f5541d6a2bfd3
hmatts3 <hans.matta@gmail.com>
2025-02-22 15:31:47 -0800

    add book practice files to repo

---
+++ myscripts.js
@@ -0,0 +1,17 @@
+function Book (title, author, pages, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+    this.info = function() {
+        return (this.title + " by " + this.author + ", " + this.pages + ", " + this.readStatus)
+    };
+}
+
+const theHobbit = new Book ('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
+
+console.log(theHobbit.title);
+
+theHobbit.info();
+
+console.log(theHobbit.info());
